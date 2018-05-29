f12ed58ab237947ae307a569d847a3e716a92683
Kristen Chellis and Matt Miller <matt@>
2018-05-29 11:27:34 -0700

    Follow lesson to create and display post list.

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -20,4 +20,24 @@ public class PostTest {
         assertEquals("Day 1: Intro", post.getContent());
 
     }
+
+    @After
+    public void tearDown() {
+        Post.clearAllPosts(); //clear out allll the posts before each test.
+    }
+
+    @Test
+    public void AllPostsAreCorrectlyReturned_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertEquals(2, Post.getAll().size());
+    }
+
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
+    }
 }
